d994a601b0d29bf7bb6afb5e29f45fbf5f5fc870
e.skrilnikov <e.skrilnikov@artjoker.team>
2021-11-17 21:18:33 +0200

    countries

--- database/seeds/ReviewSeeder.php
+++ database/seeds/ReviewSeeder.php
@@ -11,6 +11,6 @@ class ReviewSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Models\Review::class, 25)->create();
+        factory(\App\Models\Review::class, 100)->create();
     }
 }

40e78a788ea5ebb0c3568c61614d19dbb013acac
e.skrilnikov <e.skrilnikov@artjoker.team>
2022-07-09 10:17:32 +0300

    bash

--- bash.sh
+++ bash.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/bin/php /home/u771985207/domains/reviews4results.com/reviews4results/artisan schedule:run 1>> /dev/null 2>&1
-/usr/bin/php /home/u771985207/domains/reviews4results.com/reviews4results/artisan queue:work --queue=high,default
+/usr/bin/php /home/u771985207/domains/reviews4arab.com/reviews4arab/artisan schedule:run 1>> /dev/null 2>&1
+/usr/bin/php /home/u771985207/domains/reviews4arab.com/reviews4arab/artisan queue:work --queue=high,default
